d0822d55cedcb91b12424be9150380a7c17e27f7
kraxarn <me@kraxarn.com>
2019-11-11 21:12:57 +0100

    Initial code

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char *argv[])
+{
+	// Enable high-dpi scaling
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+	// Create main application
+	QGuiApplication app(argc, argv);
+
+	// Set QtQuick style
+	QQuickStyle::setStyle("Material");
+
+	// Load main engine
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+					 &app, [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	engine.load(url);
+
+	// Run while app is running
+	return app.exec();
+}
