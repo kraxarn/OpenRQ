f3ad644b5f4cb7cac0d432126a93296f8064de0a
kraxarn <me@kraxarn.com>
2019-11-27 14:19:54 +0100

    Update for new paths

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	// Load main engine
 	QQmlApplicationEngine engine;
-	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 					 &app, [url](QObject *obj, const QUrl &objUrl) {
 		if (!obj && url == objUrl)
