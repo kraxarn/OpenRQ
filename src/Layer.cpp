95dd33bc2694c2fdf8411a803a6219d49e9abea8
unknown <mabutko@riteh.hr>
2019-11-27 19:36:29 +0100

    LayerV2

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -4,3 +4,23 @@ Layer::Layer()
 {
 
 }
+
+bool Layer::saveChanges(){
+	bool i = false;
+	return i;
+}
+
+bool Layer::compare(){ //argument type? - version
+	bool i = false;
+	return i;
+}
+
+bool Layer::createItem(){
+	bool i = false;
+	return i;
+}
+
+bool Layer::deleteItem(){ //argument int - ID
+	bool i = false;
+	return i;
+}
--- src/Layer.h
+++ src/Layer.h
@@ -1,11 +1,21 @@
-#ifndef LAYER_H
+#ifndef LAYER_H //#ifdef if macro is declared
 #define LAYER_H
 
+#include <QString>
 
 class Layer
 {
 public:
 	Layer();
+	bool saveChanges();
+	bool compare(); //argument type? - version
+	bool createItem();
+	bool deleteItem(); //argument int - ID
+	QString tag;
+	bool shown;
+
+private:
+	int id;
 };
 
 #endif // LAYER_H
