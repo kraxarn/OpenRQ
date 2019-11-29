70e52116189e591b204f51958ad044a771353c4a
kraxarn <me@kraxarn.com>
2019-11-29 16:00:43 +0100

    Initial database creation

--- src/datacontext.cpp
+++ src/datacontext.cpp
@@ -33,12 +33,121 @@ DataContext::~DataContext()
 bool DataContext::create()
 {
 	QSqlQuery query(database);
-	return query.exec("create table Info ("
-					  "id integer primary key default 0,"
-					  "version integer,"
-					  "name text,"
-					  "create integer default current_timestamp"
-					  ")");
+
+	// Create Info table
+	if (!query.exec(
+		"create table Info ("
+		"	id integer primary key default 0,"
+		"	version integer default 0,"
+		"	name text,"
+		"	created integer default current_timestamp"
+		")"))
+	{
+        qCritical() << "database error: failed to create Info table";
+		return false;
+	}
+
+	// Create Solutions table
+	if (!query.exec(
+		"create table Solutions ("
+		"	id integer primary key autoincrement,"
+		"	parent integer,"
+		"	label integer,"
+		"	description text,"
+		"	link text,"
+		"	foreign key(parent) references Requirements(id),"
+		"	foreign key(label) references Labels(id)"
+		")"))
+	{
+        qCritical() << "database error: failed to create Solution table";
+		return false;
+	}
+	
+		// Create Info project
+	if (!query.exec(
+		"create table Projects ("
+		"	id integer primary key default 0,"
+		"	name text,"
+		"	created integer default current_timestamp"
+		")"))
+	{
+        qCritical() << "database error: failed to create Info projects table";
+		return false;
+	}
+	
+	// Create ItemVersions table
+	if (!query.exec(
+		"create table ItemVersions ("
+        "	id integer primary key autoincrement,"
+        "   version integer,"
+		"	item integer,"
+		"	type integer,"
+        "	foreign key (version) references Projects(id)"
+		")"))
+    {
+        qCritical() << "database error: failed to create ItemVersions table";
+		return false;
+	}
+
+	if (!query.exec(
+		"create table Requirements ("
+		"	id integer primary key autoincrement,"
+		"	parent integer,"
+		"	label integer,"
+		"	description text,"
+		"	rationale text,"
+		"	fitCriterion text,"
+		"	foreign key(parent) references Solutions(id),"
+		"	foreign key(label) references Labels(id)"
+		")"))
+	{
+        qCritical() << "database error: failed to create Requirements table";
+		return false;
+	}
+
+		// Create Info LableItem
+	if (!query.exec(
+		"create table LabelItems ("
+		"	id integer primary key default 0,"
+        "	label integer,"
+		"	item integer,"
+		"	type integer,"
+		"	foreign key (label) references Labels(id)"
+		")"))
+	{
+        qCritical() << "database error: failed to create LabelItems table";
+		return false;
+	}
+
+	
+	// Create Media table
+	if (!query.exec(
+		"create table Media ("
+		"	id integer primary key default 0,"
+		"	parent int not null,"
+		"	format text default 'webp',"
+		"	data blob,"
+		"	foreign key(parent) references Solutions(id)"
+		")"))
+	{
+        qCritical() << "database error: failed to create Media table";
+		return false;
+	}
+	
+	//Create Label table
+		if(!query.exec(
+			"create table Labels ("
+			"	id integer primary key default 0,"
+			"	tag text,"
+			"	color integer"
+			")"))
+	{
+        qCritical() << "database error: failed to create Label table";
+		return false;
+	}
+
+	// All good
+	return true;
 }
 
 bool DataContext::isOpen()
