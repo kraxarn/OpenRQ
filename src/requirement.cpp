f66763997a0f84460efabd95f8d1bd65f2aabb22
kraxarn <me@kraxarn.com>
2019-11-30 11:56:28 +0100

    Added non implemented methods for later

--- src/requirement.cpp
+++ src/requirement.cpp
@@ -9,12 +9,27 @@ namespace orq
 
 	Requirement::Requirement(QSqlQuery query)
 	{
-
+		qFatal("error: Requirement(QSQlQuery) is not implemented");
 	}
 
-	Requirement::~Requirement()
+	bool Requirement::saveChanges()
 	{
+		qFatal("error: Requirement::saveChanges() is not implemented");
+	}
 
+	QVector<Item*> Requirement::getChildren()
+	{
+		qFatal("error: Requirement::getChildren() is not implemented");
+	}
+
+	Item& Requirement::getParent()
+	{
+		qFatal("error: Requirement::getParent() is not implemented");
+	}
+
+	bool Requirement::setParent(Item &item)
+	{
+		qFatal("error: Requirement::setParent(Item&) is not implemented");
 	}
 
 	QByteArray Requirement::getHash()
--- src/requirement.h
+++ src/requirement.h
@@ -11,15 +11,31 @@ namespace orq
 	class Requirement : public Item
 	{
 	public:
+		/// Create a new empty requirement
 		Requirement();
 
+		/// Construct a requirement from a SQL query
 		Requirement(QSqlQuery query);
 
-		~Requirement();
-
+		/// Requirement rationale
 		QString rationale;
+
+		/// Requirement fit criterion
 		QString fitCriterion;
 
+		/// Item::saveChanges()
+		bool saveChanges();
+
+		/// Item::getChildren()
+		QVector<Item*> getChildren();
+
+		/// Item::getParent()
+		Item &getParent();
+
+		/// Item::setParent(Item)
+		bool setParent(Item &item);
+
+		/// Item::getHash()
 		QByteArray getHash();
 	};
 }
