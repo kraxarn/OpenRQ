c980dc9f572858fb51da74773af1d4c42d6f67da
kraxarn <me@kraxarn.com>
2019-12-02 22:45:58 +0100

    New logic for creating items
    Co-authored-by: Haxwell1 <hawkar.karim@hotmail.com>

--- src/datacontext.cpp
+++ src/datacontext.cpp
@@ -117,6 +117,46 @@ namespace orq
 		// Prepare query
 		QSqlQuery query(database);
 
+		// Create new if it doesn't exist
+		if (item.uid == 0)
+		{
+			item.uid = getItemUid();
+			int itemId = 0;
+
+			if (type == TypeRequirement)
+			{
+				auto req = dynamic_cast<Requirement&>(item);
+
+				query.prepare("insert into Requirements (uid, description, rationale, fitCriterion) "
+					"values (:uid, :description, :rationale, :fitCriterion)");
+				query.bindValue(":uid", item.uid);
+				query.bindValue(":description", item.description);
+				query.bindValue(":rationale", req.rationale);
+				query.bindValue(":fitCriterion", req.fitCriterion);
+			}
+			else
+			{
+				query.prepare("insert into Solutions (uid, description) "
+					"values (:uid, :description)");
+				query.bindValue(":uid", item.uid);
+				query.bindValue(":description", item.description);
+			}
+
+			if (!query.exec())
+				return false;
+
+			query.prepare("select id from Requirements where uid = :uid");
+			query.exec();
+			itemId = query.value(0).toInt();
+			
+			query.prepare("insert into ItemVersions (version, item, type) values (:version, :item, :type)");
+			query.bindValue(":version", projectVersion);
+			query.bindValue(":item", itemId);
+			query.bindValue(":type", type == TypeRequirement ? "Requirements" : "Solutions");
+
+			return query.exec();
+		}
+
 		// Check if already pending version
 		query.prepare("select * from ItemVersions where version = :version and item = :item");
 		query.bindValue(":version", projectVersion);
