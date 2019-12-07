0fb65a314c2c769a73745df4ed36b88bc7fe5316
kraxarn <me@kraxarn.com>
2019-12-07 16:30:22 +0100

    Initial menu and tool bars

--- src/main.go
+++ src/main.go
@@ -15,6 +15,10 @@ func main() {
 	window.SetMinimumSize2(1280, 720)
 	window.SetWindowTitle("OpenRQ")
 
+	// Add menu bar
+	AddMenuBars(window)
+	AddToolBars(window)
+
 	// make the window visible
 	window.Show()
 
@@ -23,4 +27,33 @@ func main() {
 
 	// start the main Qt event loop
 	app.Exec()
+}
+
+func AddMenuBars(window *widgets.QMainWindow) {
+	// File menu
+	fileMenu := window.MenuBar().AddMenu2("File")
+	fileMenu.AddAction("New...")
+	fileMenu.AddAction("Open...")
+	fileMenu.AddAction("Save")
+	fileMenu.AddAction("Save As...")
+	fileMenu.AddSeparator()
+	fileMenu.AddAction("Close")
+	// Add menu
+	addMenu := window.MenuBar().AddMenu2("Add")
+	addMenu.AddAction("Requirement")
+	addMenu.AddAction("Solution")
+	// View menu
+	viewMenu := window.MenuBar().AddMenu2("View")
+	viewMenu.AddAction("Validation Engine")
+}
+
+func AddToolBars(window *widgets.QMainWindow) {
+	fileToolBar := window.AddToolBar3("")
+	fileToolBar.SetMovable(false)
+	fileToolBar.AddAction("New Requirement")
+	fileToolBar.AddAction("New Solution")
+	spacer := widgets.NewQWidget(nil, 0)
+	spacer.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Expanding)
+	fileToolBar.AddWidget(spacer)
+	a := fileToolBar.AddAction("Validation Engine")
 }
