926bb7e713f06331a232050a2d018d302e1c407c
kraxarn <me@kraxarn.com>
2019-12-07 21:23:14 +0100

    Make validation engine toggle checkable

--- src/main.go
+++ src/main.go
@@ -55,5 +55,5 @@ func AddToolBars(window *widgets.QMainWindow) {
 	spacer := widgets.NewQWidget(nil, 0)
 	spacer.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Expanding)
 	fileToolBar.AddWidget(spacer)
-	a := fileToolBar.AddAction("Validation Engine")
+	fileToolBar.AddAction("Validation Engine").SetCheckable(true)
 }
