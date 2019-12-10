81e5f1a66a6fd1664faa567ae5c82a73e026e0a7
kraxarn <me@kraxarn.com>
2019-12-11 00:08:00 +0100

    Method for adding items
    Co-authored-by: Haxwell1 <hawkar.karim@hotmail.com>
    Co-authored-by: GSone <Georgesarossy@hotmail.com>

--- datacontext.go
+++ datacontext.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 
@@ -71,6 +72,59 @@ func (data *DataContext) Create(projectName string) bool {
 	}
 
 	return true
+}
+
+func (data *DataContext) UpdateItem(item Item, projectVersion int) error {
+	// Try to cast item as requirement
+	req, isReq := item.(Requirement)
+	// Get requirement/solution as string
+	typeName := "s"
+	if isReq {
+		typeName = "r"
+	}
+	// Prepare query
+	query := sql.NewQSqlQuery3(data.Database)
+	// Create if it doesn't exist
+	if item.GetUid() == 0 {
+		item.SetUid(data.GetItemUid())
+		if isReq {
+			// Add requirement
+			query.Prepare("insert into Requirements (uid, description, rationale, fitCriterion) values (:uid, :description, :rationale, :fitCriterion)")
+			query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
+			query.BindValue(":description", core.NewQVariant12(item.GetDescription()), sql.QSql__In)
+			query.BindValue(":rationale", core.NewQVariant12(req.GetRationale()), sql.QSql__In)
+			query.BindValue(":fitCriterion", core.NewQVariant12(req.GetFitCriterion()), sql.QSql__In)
+		} else {
+			// Add solution
+			query.Prepare("insert into Solutions (uid, description) values (:uid, :description)")
+			query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
+			query.BindValue(":description", core.NewQVariant12(item.GetDescription()), sql.QSql__In)
+		}
+
+		// Try to add item
+		if !query.Exec2() {
+			return fmt.Errorf("failed to create item: %v", data.Database.LastError().Text())
+		}
+
+		// Find item id
+		query.Prepare("select id from Requirements where uid = :uid")
+		query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
+		if !query.Exec2() {
+			return fmt.Errorf("failed to get item id: %v", data.Database.LastError().Text())
+		}
+
+		query.Prepare("insert into ItemVersions (version, item, type) values (:version, :item, :type)")
+		query.BindValue(":version", core.NewQVariant5(item.GetVersion()), sql.QSql__In)
+		query.BindValue(":item", core.NewQVariant5(item.GetId()), sql.QSql__In)
+		query.BindValue(":type", core.NewQVariant12(typeName), sql.QSql__In)
+
+		if !query.Exec2() {
+			return fmt.Errorf("failed to get create item version: %v", data.Database.LastError().Text())
+		}
+	}
+
+	// Rest is not completed yet
+	return nil
 }
 
 func UidExists(query *sql.QSqlQuery, uid int64) bool {
