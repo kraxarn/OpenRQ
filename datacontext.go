d30d328344af2742fe46dbdbae3225b5d73d31b8
kraxarn <me@kraxarn.com>
2019-12-11 15:48:44 +0100

    Initial rewrite to database/sql
    Co-authored-by: GSone <Georgesarossy@hotmail.com>

--- datacontext.go
+++ datacontext.go
@@ -1,48 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"strings"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/sql"
 )
 
+// DataContext is the context to the database
 type DataContext struct {
-	Database *sql.QSqlDatabase
+	Database *sql.DB
 }
 
+// NewDataContext creates a new database
 func NewDataContext(path string) *DataContext {
 	data := new(DataContext)
-
-	// Check if SQLite is available
-	if !sql.QSqlDatabase_IsDriverAvailable("QSQLITE") {
-		fmt.Fprintln(os.Stderr, "error: sqlite is not available")
-		return nil
-	}
 
 	// Check beforehand if file exists
 	fileExists := core.QFile_Exists(path)
 
 	// Create SQLite database
-	data.Database = sql.QSqlDatabase_AddDatabase("QSQLITE", "")
-	data.Database.SetDatabaseName(path)
+	var err error
+	data.Database, err = sql.Open("sqlite3", path)
+	defer data.Database.Close()
 
 	// Open file
-	if !data.Database.Open() {
-		fmt.Fprintln(os.Stderr, "error: failed to open database")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error: failed to open database:", err)
+		return nil
 	}
 
 	// Create if it didn't exist
 	if !fileExists {
-		fileName := core.NewQFileInfo3(path).FileName()
+		fileName := filepath.Base(path)
 		if strings.Contains(fileName, ".") {
 			fileName = strings.TrimLeft(fileName, ".")
 		}
-		if !data.Create(fileName) {
-			fmt.Fprintln(os.Stderr, "error: failed to create initial database")
+		if err := data.Create(fileName); err != nil {
+			fmt.Fprintln(os.Stderr, "error: failed to create database:", err)
 			return nil
 		}
 	}
@@ -50,30 +50,25 @@ func NewDataContext(path string) *DataContext {
 	return data
 }
 
-func (data *DataContext) Create(projectName string) bool {
-	// Prepare query
-	query := sql.NewQSqlQuery2("", data.Database)
-
+// Create creates a new, empty database
+func (data *DataContext) Create(projectName string) error {
 	// Loop through table data
 	for key, value := range tableData {
 		// Execute query
-		if !query.Exec(fmt.Sprintf("create table %s (%s)", key, strings.Join(value, ", "))) {
-			fmt.Fprintln(os.Stderr, "database error: failed to create", key, "table")
-			return false
+		_, err := data.Database.Exec(fmt.Sprintf("create table %s (%s)", key, strings.Join(value, ", ")))
+		// Check for errors
+		if err != nil {
+			return fmt.Errorf("error: failed to create %v table", key)
 		}
 	}
 
 	// Insert info table
-	query.Prepare("insert into Info (name) values (:name)")
-	query.BindValue(":name", core.NewQVariant12(projectName), sql.QSql__In)
-	if !query.Exec2() {
-		fmt.Fprintln(os.Stderr, "database error: failed to insert into Info table")
-		return false
-	}
-
-	return true
+	stmt, err := data.Database.Prepare("insert into Info (name) values (?)")
+	stmt.QueryRow(projectName)
+	return err
 }
 
+// UpdateItem updates an already existing item or creates a new one
 func (data *DataContext) UpdateItem(item Item, projectVersion int) error {
 	// Try to cast item as requirement
 	req, isReq := item.(Requirement)
@@ -82,44 +77,42 @@ func (data *DataContext) UpdateItem(item Item, projectVersion int) error {
 	if isReq {
 		typeName = "r"
 	}
-	// Prepare query
-	query := sql.NewQSqlQuery3(data.Database)
 	// Create if it doesn't exist
 	if item.GetUid() == 0 {
 		item.SetUid(data.GetItemUid())
 		if isReq {
 			// Add requirement
-			query.Prepare("insert into Requirements (uid, description, rationale, fitCriterion) values (:uid, :description, :rationale, :fitCriterion)")
-			query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
-			query.BindValue(":description", core.NewQVariant12(item.GetDescription()), sql.QSql__In)
-			query.BindValue(":rationale", core.NewQVariant12(req.GetRationale()), sql.QSql__In)
-			query.BindValue(":fitCriterion", core.NewQVariant12(req.GetFitCriterion()), sql.QSql__In)
+			stmt, err := data.Database.Prepare("insert into Requirements (uid, description, rationale, fitCriterion) values (?, ?, ?, ?)")
+			stmt.QueryRow(item.GetUid(), item.GetDescription(), req.GetRationale(), req.GetFitCriterion())
+			if err != nil {
+				return fmt.Errorf("failed to insert requirement: %v", err)
+			}
 		} else {
 			// Add solution
-			query.Prepare("insert into Solutions (uid, description) values (:uid, :description)")
-			query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
-			query.BindValue(":description", core.NewQVariant12(item.GetDescription()), sql.QSql__In)
-		}
-
-		// Try to add item
-		if !query.Exec2() {
-			return fmt.Errorf("failed to create item: %v", data.Database.LastError().Text())
+			stmt, err := data.Database.Prepare("insert into Solutions (uid, description) values (?, ?)")
+			stmt.QueryRow(item.GetUid(), item.GetDescription())
+			if err != nil {
+				return fmt.Errorf("failed to insert solution: %v", err)
+			}
 		}
 
 		// Find item id
-		query.Prepare("select id from Requirements where uid = :uid")
-		query.BindValue(":uid", core.NewQVariant7(item.GetUid()), sql.QSql__In)
-		if !query.Exec2() {
-			return fmt.Errorf("failed to get item id: %v", data.Database.LastError().Text())
+		stmt, err := data.Database.Prepare("select id from Requirements where uid = ?")
+		row := stmt.QueryRow(item.GetUid())
+		if err != nil {
+			return fmt.Errorf("error: failed to get item id: %v", err)
+		}
+		var itemID int
+		err = row.Scan(&itemID)
+		if err != nil || itemID == 0 {
+			return fmt.Errorf("error: failed to get item id: %v", err)
 		}
 
-		query.Prepare("insert into ItemVersions (version, item, type) values (:version, :item, :type)")
-		query.BindValue(":version", core.NewQVariant5(item.GetVersion()), sql.QSql__In)
-		query.BindValue(":item", core.NewQVariant5(item.GetId()), sql.QSql__In)
-		query.BindValue(":type", core.NewQVariant12(typeName), sql.QSql__In)
+		stmt, err = data.Database.Prepare("insert into ItemVersions (version, item, type) values (?, ?, ?)")
+		row = stmt.QueryRow(item.GetVersion(), itemID, typeName)
 
-		if !query.Exec2() {
-			return fmt.Errorf("failed to get create item version: %v", data.Database.LastError().Text())
+		if err != nil {
+			return fmt.Errorf("error: failed to get create item version: %v", err)
 		}
 	}
 
@@ -130,6 +123,7 @@ func (data *DataContext) UpdateItem(item Item, projectVersion int) error {
 //Discussion needed
 
 //AddChild adding children to the tree
+/*
 func (data *DataContext) AddChild(item Item, child int, root int) {
 	query := sql.NewQSqlQuery2("", data.Database)
 
@@ -147,28 +141,29 @@ func (data *DataContext) RemoveChild(item Item, child int, root int) {
 	query.BindValue(":itemId", core.NewQVariant1(item.GetId()), sql.QSql__In)
 
 }
+*/
 
-//UidExists checking if the specified uid is already taken
-func UidExists(query *sql.QSqlQuery, uid int64) bool {
+// UidExists checking if the specified uid is already taken
+func UidExists(db *sql.DB, uid int64) bool {
 	// Prepare union query
-	query.Prepare("select count(*) from (select uid from Requirements union select uid from Solutions) where uid = :uid")
+	stmt, err := db.Prepare("select count(*) from (select uid from Requirements union select uid from Solutions) where uid = ?")
 	// Bind uid
-	query.BindValue(":uid", core.NewQVariant7(uid), sql.QSql__In)
-	// Execute and get result
-	query.Exec2()
-	query.First()
+	var count int
+	stmt.QueryRow(uid).Scan(&count)
+	// Check for error
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "warning: failed to get uid: %v", err)
+	}
 	// If count is above 0, row is found
-	return query.Value(0).ToInt(nil) > 0
+	return count > 0
 }
 
 //GetItemUid If the item uid dosn't exits we will get a uid
 func (data *DataContext) GetItemUid() int64 {
-	// Prepare query
-	query := sql.NewQSqlQuery3(data.Database)
 	// Generate initial id
 	id := int64(rand.Uint64())
 	// Keep generating until unique
-	for UidExists(query, id) {
+	for UidExists(data.Database, id) {
 		id = int64(rand.Uint64())
 	}
 	// Return newly generated value
