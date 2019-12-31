97aa2ccad261b2a9a361e7b13bc9d3d8d13446ce
kraxarn <me@kraxarn.com>
2019-12-31 16:17:47 +0100

    Set required values for QSettings

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
+import "github.com/therecipe/qt/core"
+
 // Variables set from linker flags
 var versionTagName, versionCommitHash string
 
 func main() {
+	// Setup some application variables
+	core.QCoreApplication_SetOrganizationName("kraxarn")
+	core.QCoreApplication_SetOrganizationDomain("kraxarn.com")
+	core.QCoreApplication_SetApplicationName("OpenRQ")
+
 	// Create window and main app
 	app, window := NewMainWindow()
 
