c329a2f1a41d55a98020f50724c8bcbf8fc80507
kraxarn <me@kraxarn.com>
2019-12-31 14:37:36 +0100

    Add support for GZip

--- project.go
+++ project.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -34,6 +37,25 @@ func (proj *Project) Name() string {
 	return db.ProjectName()
 }
 
+func Compress(data []byte) ([]byte, error) {
+	var buffer bytes.Buffer
+	gz := gzip.NewWriter(&buffer)
+	count, err := gz.Write(data)
+	_ = gz.Close()
+	fmt.Println("wrote", count, "/", len(buffer.Bytes()), "bytes")
+	return buffer.Bytes(), err
+}
+
+func Decompress(data []byte) ([]byte, error) {
+	buffer := bytes.NewBuffer(data)
+	gz, err := gzip.NewReader(buffer)
+	if err != nil {
+		return nil, err
+	}
+	_ = gz.Close()
+	return ioutil.ReadAll(gz)
+}
+
 func (proj *Project) CopyTo(path string) error {
 	// Get file info to copy permissions
 	fileInfo, err := os.Stat(proj.path)
@@ -44,6 +66,15 @@ func (proj *Project) CopyTo(path string) error {
 	file, err := ioutil.ReadFile(proj.path)
 	if err != nil {
 		return err
+	}
+	// GZip
+	if strings.HasSuffix(path, ".orqz") {
+		temp, err := Compress(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "warning: failed to compress data:", err)
+		} else {
+			file = temp
+		}
 	}
 	// Copy data to location
 	return ioutil.WriteFile(path, file, fileInfo.Mode())
