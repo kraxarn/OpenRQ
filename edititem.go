1c55e01a9c5cd10a9f6d5374f69b6baf6a62f14e
kraxarn <me@kraxarn.com>
2020-01-02 14:40:11 +0100

    Add temporary input for solution attachments

--- edititem.go
+++ edititem.go
@@ -93,10 +93,12 @@ func CreateEditWidget(item Item, group *widgets.QGraphicsItemGroup, scene *widge
 	reqRadio.ConnectReleased(func() {
 		textGroups[1].Show()
 		textGroups[2].Show()
+		textGroups[3].Hide()
 	})
 	solRadio.ConnectReleased(func() {
 		textGroups[1].Hide()
 		textGroups[2].Hide()
+		textGroups[3].Show()
 	})
 
 	// TODO: Assume requirement for now
@@ -182,8 +184,17 @@ func CreateEditWidget(item Item, group *widgets.QGraphicsItemGroup, scene *widge
 		textGroups[i] = CreateGroupBox(titles[i], textOptions[i], textEdits[i])
 		layout.AddWidget(textGroups[i], 1, 0)
 	}
+
+	// Add image selection for solution
+	// TODO: Temporary placeholder text
+	noImagesText := widgets.NewQLabel2("No attachments", nil, core.Qt__Widget)
+	noImagesText.SetEnabled(false)
+	textGroups[3] = CreateGroupBox("Attachments", noImagesText)
+	layout.AddWidget(textGroups[3], 0, 0)
+
 	// Hide stuff
 	if itemType == TypeRequirement {
+		textGroups[3].Hide()
 	} else {
 		textGroups[1].Hide()
 		textGroups[2].Hide()
