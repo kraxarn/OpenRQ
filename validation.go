64ec3cd87f904a5970cbc0f09b5457b951613d31
kraxarn <me@kraxarn.com>
2020-01-03 20:35:18 +0100

    Initial validation engine

---
+++ validation.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
+	"github.com/therecipe/qt/widgets"
+)
+
+// Validates link to check that links are not the same type
+func ValidateLinks() (items []Item) {
+	items = make([]Item, 0)
+	for _, link := range links {
+		for _, l := range link {
+			if GetItemType(l.parent) == GetItemType(l.child) {
+				items = append(items, l.child)
+			}
+		}
+	}
+	return items
+}
+
+// Validates roots to check if they have a one-to-one relation
+func ValidateRoots() (items []Item) {
+	items = make([]Item, 0)
+	for _, item := range view.Items() {
+		group := item.Group()
+		if group == nil || group.Type() == 0 {
+			continue
+		}
+		// TODO
+	}
+	return items
+}
+
+// Validates tree to check if there are any links to each other
+func ValidateLoops() (items []Item) {
+	// TODO
+	return nil
+}
+
+type ValidationOption int8
+const (
+	SameType ValidationOption = 0
+	OneRoot  ValidationOption = 1
+	LinkLoop ValidationOption = 2
+)
+
+type ValidationResult string
+const (
+	ValidateOK       ValidationResult = "emblem-checked"
+	ValidateFail     ValidationResult = "emblem-error"
+	ValidateDisabled ValidationResult = "emblem-pause"
+	ValidateNone     ValidationResult = "emblem-question"
+)
+
+func CreateValidationResult(option ValidationOption, result ValidationResult) *widgets.QListWidgetItem {
+	var text, info string
+	switch option {
+	case SameType:
+		text = "Links to same type"
+		info = "Linking items of the same type"
+	case OneRoot:
+		text = "One-to-one root"
+		info = "Roots of the current tree having an one-to-one relation to its child"
+	case LinkLoop:
+		text = "Linking loop"
+		info = "Items that link to each other in a loop"
+	}
+	item := widgets.NewQListWidgetItem3(gui.QIcon_FromTheme(string(result)), text, nil, 0)
+	item.SetToolTip(info)
+	return item
+}
+
+func GetDefaultValidationResult(enabled bool) ValidationResult {
+	if enabled {
+		return ValidateNone
+	}
+	return ValidateDisabled
+}
+
+// CreateValidationEngineLayout creates the validation engine window
+func CreateValidationEngineLayout() *widgets.QWidget {
+	enabled := []bool{
+		true, true, true,
+	}
+	// Main vertical box
+	layout := widgets.NewQVBoxLayout()
+	// Option to validate
+	runBtn := widgets.NewQPushButton2("Run now", nil)
+	runBtn.ConnectReleased(func() {
+		runBtn.SetText("Running...")
+		runBtn.SetEnabled(false)
+		// ...
+	})
+	layout.AddWidget(runBtn, 0, 0)
+
+	/*
+	 * List icons:
+	 * ok:		emblem-checked
+	 * fail:	emblem-error
+	 * off:		emblem-pause
+	 * none:	emblem-question
+	 */
+
+	results := widgets.NewQListWidget(nil)
+	for i, e := range enabled {
+		results.AddItem2(CreateValidationResult(ValidationOption(i), GetDefaultValidationResult(e)))
+	}
+	layout.AddWidget(CreateGroupBox("Last validation: never", results), 1, 0)
+
+	results.ConnectItemPressed(func(item *widgets.QListWidgetItem) {
+		i := results.Row(item)
+		menu := widgets.NewQMenu(nil)
+		action := menu.AddAction("Enabled")
+		action.SetCheckable(true)
+		action.SetChecked(enabled[i])
+		action.ConnectTriggered(func(checked bool) {
+			enabled[i] = action.IsChecked()
+			item.SetIcon(gui.QIcon_FromTheme(string(GetDefaultValidationResult(enabled[i]))))
+		})
+		menu.Popup(gui.QCursor_Pos(), nil)
+	})
+
+	items := widgets.NewQListWidget(nil)
+	noItems := widgets.NewQListWidgetItem2("No items to show", nil, 0)
+	noItems.SetFlags(0)
+	itemGroup := CreateGroupBox("Affected Items", items)
+	layout.AddWidget(itemGroup, 1, 0)
+
+	// Convert layout to widget and return it
+	widget := widgets.NewQWidget(nil, core.Qt__Widget)
+	widget.SetLayout(layout)
+	widget.SetMaximumWidth(250)
+	widget.SetMinimumWidth(175)
+	return widget
+}
