7f336b0cd5dbaec33ce4f054fdc7da87255c50b6
kraxarn <me@kraxarn.com>
2020-01-12 00:02:00 +0100

    Link unit testing, fixes #23

--- tree_test.go
+++ tree_test.go
@@ -30,12 +30,46 @@ func TestTree(t *testing.T) {
 	db := project.Data()
 	// Check so that we have no items
 	if count := len(Roots()); count != 0 {
-		t.Error("unexpected item count, expected 0, but found", count)
+		t.Error("unexpected item count, expected 0, but got", count)
 	}
 	// Add a new requirement
-	_, err = db.AddEmptyRequirement()
+	req1id, err := db.AddEmptyRequirement()
 	if err != nil {
 		t.Error("failed to add requirement 1:", err)
+	}
+	req1 := NewRequirement(req1id)
+	// Make sure the requirement description is empty
+	if descLen := len(req1.Description()); descLen != 0 {
+		t.Error("unexpected requirement 1 description length, expected 0, but got", descLen)
+	}
+	// Try to set a description
+	req1desc := "requirement 1 description"
+	req1.SetDescription(req1desc)
+	if req1.Description() != req1desc {
+		t.Errorf("unexpected requirement 1 description, expected \"%v\", but got \"%v\"",
+			req1desc, req1.Description())
+	}
+	// Make sure the requirement doesn't have any children
+	if count := len(req1.Children()); count != 0 {
+		t.Error("unexpected requirement 1 children count, expected 0, but got", count)
+	}
+	// Create a new solution
+	sol1id, err := db.AddEmptySolution()
+	if err != nil {
+		t.Error("failed to add solution 1:", err)
+	}
+	sol1 := NewSolution(sol1id)
+	// Make sure solution 1 has no parent
+	if sol1.Parent() != nil {
+		t.Error("unexpected solution 1 parent, expected nil, but got", sol1.Parent().ToString())
+	}
+	// Link requirement 1 to solution 1
+	if err = db.AddItemChild(req1, sol1); err != nil {
+		t.Error("failed to create link between requirement 1 and solution 1:", err)
+	}
+	// Make sure solution 1 has requirement 1 as parent
+	if sol1.Parent() != req1 {
+		t.Error("unexpected solution 1 parent, expected requirement 1, but got", sol1.Parent().ToString())
 	}
 	// Close database
 	if err = db.Close(); err != nil {
