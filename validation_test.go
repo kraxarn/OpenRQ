8793cd37fbfd253e480d9dc73acf8fc46c7a0644
kraxarn <me@kraxarn.com>
2020-01-13 22:25:25 +0100

    Unit testing for link error validation

--- validation_test.go
+++ validation_test.go
@@ -6,9 +6,6 @@ import (
 	"testing"
 )
 
-// ValidateLinkErrors can't be easily tested
-// as they depend on the graphics view
-
 func TestValidateLinks(t *testing.T) {
 	links = make(map[Item][]*Link)
 	// Create two items of different types
@@ -97,6 +94,39 @@ func TestValidateRoots(t *testing.T) {
 	CreateLink(req1, sol2)
 	// Validation should now fail
 	if links := len(ValidateRoots()); links != 1 {
+		t.Error("unexpected validation result, expected 1 error, but got", links)
+	}
+}
+
+func TestValidateLinkErrors(t *testing.T) {
+	// ValidateRoots depend on a view being available
+	_, window := NewMainWindow()
+	CreateLayout(window)
+	// Reset project if any was loaded
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error(err)
+	}
+	NewProject(fmt.Sprintf("%v/openrq_test.orq", tmpDir))
+	ReloadProject(window)
+	// Create two items of different types
+	req1 := NewGraphicsItem("req1", 0, 0, 0, 0, NewRequirement(1))
+	sol1 := NewGraphicsItem("sol1", 0, 0, 0, 0, NewSolution(1))
+	// Add to scene
+	scene.AddItem(req1)
+	scene.AddItem(sol1)
+	// Create a link between the items
+	CreateLink(req1, sol1)
+	// Validation should be successful
+	if links := len(ValidateLinkErrors()); links != 0 {
+		t.Error("unexpected validation result, expected 0 errors, but got", links)
+	}
+	// Add another parent
+	req2 := NewGraphicsItem("req2", 0, 0, 0, 0, NewRequirement(2))
+	scene.AddItem(req2)
+	CreateLink(req2, sol1)
+	// Validation should now fail
+	if links := len(ValidateLinkErrors()); links != 1 {
 		t.Error("unexpected validation result, expected 1 error, but got", links)
 	}
 }
