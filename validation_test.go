75586847e332960b85f7bdd764a2736ea9a9e0ab
kraxarn <me@kraxarn.com>
2020-01-13 22:21:20 +0100

    Unit test for root validation

--- validation_test.go
+++ validation_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
 
-// ValidateRoots and ValidateLinkErrors can't be easily tested
+// ValidateLinkErrors can't be easily tested
 // as they depend on the graphics view
 
 func TestValidateLinks(t *testing.T) {
@@ -60,6 +64,39 @@ func TestValidateLoops(t *testing.T) {
 	links[req1] = append(links[req1], &link2)
 	// Validation should fail now
 	if links := len(ValidateLoops()); links != 2 {
+		t.Error("unexpected validation result, expected 1 error, but got", links)
+	}
+}
+
+func TestValidateRoots(t *testing.T) {
+	// ValidateRoots depend on a view being available
+	_, window := NewMainWindow()
+	CreateLayout(window)
+	// Reset project if any was loaded
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error(err)
+	}
+	NewProject(fmt.Sprintf("%v/openrq_test.orq", tmpDir))
+	ReloadProject(window)
+	// Create two items of different types
+	req1 := NewGraphicsItem("req1", 0, 0, 0, 0, NewRequirement(1))
+	sol1 := NewGraphicsItem("sol1", 0, 0, 0, 0, NewSolution(1))
+	// Add to scene
+	scene.AddItem(req1)
+	scene.AddItem(sol1)
+	// Create a link between the items
+	CreateLink(req1, sol1)
+	// Validation should be successful
+	if links := len(ValidateRoots()); links != 0 {
+		t.Error("unexpected validation result, expected 0 errors, but got", links)
+	}
+	// Add another child to parent
+	sol2 := NewGraphicsItem("sol2", 0, 0, 0, 0, NewSolution(2))
+	scene.AddItem(sol2)
+	CreateLink(req1, sol2)
+	// Validation should now fail
+	if links := len(ValidateRoots()); links != 1 {
 		t.Error("unexpected validation result, expected 1 error, but got", links)
 	}
 }
